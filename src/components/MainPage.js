487b49494f3dc26814c38c61b2e57c77aee0babc
jamzheng109 <jamzheng@bu.edu>
2025-06-06 19:10:03 -0400

    main page updates

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -9,7 +9,10 @@ function MainPage() {
       <div className="main-container">
           <img src={`${process.env.PUBLIC_URL}/grad.JPG`} className="main-image"/>
           <h1>RSVP FOR DR. XI's GRADUATION PARTY!!!</h1>
-          <h1 className="main-title">Will you be attending the event on June 11 @6pm?</h1>
+          <h1 className="main-title">Will you be attending the event on June 11?</h1>
+          <h2>Address: 57 Walnut St, Quincy, MA 02170</h2>
+          <h2>Start time will be @6pm but come whenever!!!</h2>
+          <h2>Optional dress code: light blue</h2>
           <button onClick={() => navigate('/yes')} className="button button-yes">
               Yes
           </button>
